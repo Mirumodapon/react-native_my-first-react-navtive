cef4b12c958c9c1052e63fbf07e11edb02cbf394
Mirumo <mysteryperson520@gmail.com>
2021-08-23 22:37:08 +0800

    start the project sttucture

--- App.js
+++ App.js
@@ -2,45 +2,10 @@
 import React from 'react';
 
 import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 	console.log('App executed!');
 
-	return (
-		<View
-			style={{
-				backgroundColor: 'white',
-				flex: 1,
-				flexDirection: 'row',
-				justifyContent: 'center',
-				alignItems: 'center',
-				alignContent: 'center'
-			}}
-		>
-			<View
-				style={{
-					backgroundColor: 'dodgerblue',
-					width: 100,
-					height: 100
-				}}
-			/>
-			<View
-				style={{
-					backgroundColor: 'gold',
-					width: 100,
-					height: 100,
-					top: -20,
-					right: 20,
-					position: 'absolute'
-				}}
-			/>
-			<View
-				style={{
-					backgroundColor: 'tomato',
-					width: 100,
-					height: 100
-				}}
-			/>
-		</View>
-	);
+	return <WelcomeScreen />;
 }
