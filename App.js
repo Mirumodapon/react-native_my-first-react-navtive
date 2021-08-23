32774e87cc7771868f12bdac5b5dc847f34d0831
Mirumo <mysteryperson520@gmail.com>
2021-08-23 21:06:58 +0800

    add sample button

--- App.js
+++ App.js
@@ -1,59 +1,16 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-	StyleSheet,
-	Text,
-	SafeAreaView,
-	Image,
-	TouchableWithoutFeedback,
-	TouchableOpacity,
-	TouchableHighlight
-} from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
 	console.log('App executed!');
 
-	const handlePress = () => console.log('Text pressed');
+	const handlePress = () => console.log('Button pressed');
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text>Hello React Native!</Text>
-			<Text numberOfLines={1} onPress={handlePress}>
-				This is second line - A really really long text. Now i wanna
-				make this even longer and see what happens.
-			</Text>
-			<Text>Touchable without Feedback</Text>
-			<TouchableWithoutFeedback
-				onPress={() => console.log('Image tapped')}
-			>
-				<Image
-					source={{
-						url: 'https://mirumo.org/static/media/Icon.af44acbc.jpg',
-						width: 200,
-						height: 200
-					}}
-				/>
-			</TouchableWithoutFeedback>
-			<Text>Touchable Opacity</Text>
-			<TouchableOpacity onPress={() => console.log('Image tapped!')}>
-				<Image
-					source={{
-						url: 'https://mirumo.org/static/media/Icon.af44acbc.jpg',
-						width: 200,
-						height: 200
-					}}
-				/>
-			</TouchableOpacity>
-			<Text>Touchable Hightlight</Text>
-			<TouchableHighlight onPress={() => console.log('Image tapped!')}>
-				<Image
-					source={{
-						url: 'https://mirumo.org/static/media/Icon.af44acbc.jpg',
-						width: 200,
-						height: 200
-					}}
-				/>
-			</TouchableHighlight>
+			<Button title="Click me" onPress={handlePress} color="orange" />
 		</SafeAreaView>
 	);
 }
