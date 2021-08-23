d52b138280e21e948f684885309699131ebcdd54
Mirumo <mysteryperson520@gmail.com>
2021-08-23 21:03:47 +0800

    add image, and touchable components

--- App.js
+++ App.js
@@ -1,23 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	SafeAreaView,
+	Image,
+	TouchableWithoutFeedback,
+	TouchableOpacity,
+	TouchableHighlight
+} from 'react-native';
 
 export default function App() {
 	console.log('App executed!');
 
+	const handlePress = () => console.log('Text pressed');
+
 	return (
-		<View style={styles.container}>
+		<SafeAreaView style={styles.container}>
 			<Text>Hello React Native!</Text>
-			<StatusBar style="auto" />
-		</View>
+			<Text numberOfLines={1} onPress={handlePress}>
+				This is second line - A really really long text. Now i wanna
+				make this even longer and see what happens.
+			</Text>
+			<Text>Touchable without Feedback</Text>
+			<TouchableWithoutFeedback
+				onPress={() => console.log('Image tapped')}
+			>
+				<Image
+					source={{
+						url: 'https://mirumo.org/static/media/Icon.af44acbc.jpg',
+						width: 200,
+						height: 200
+					}}
+				/>
+			</TouchableWithoutFeedback>
+			<Text>Touchable Opacity</Text>
+			<TouchableOpacity onPress={() => console.log('Image tapped!')}>
+				<Image
+					source={{
+						url: 'https://mirumo.org/static/media/Icon.af44acbc.jpg',
+						width: 200,
+						height: 200
+					}}
+				/>
+			</TouchableOpacity>
+			<Text>Touchable Hightlight</Text>
+			<TouchableHighlight onPress={() => console.log('Image tapped!')}>
+				<Image
+					source={{
+						url: 'https://mirumo.org/static/media/Icon.af44acbc.jpg',
+						width: 200,
+						height: 200
+					}}
+				/>
+			</TouchableHighlight>
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
+		backgroundColor: 'white',
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 });
