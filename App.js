ca00ca1db246c43a345affb716bfa746c64d7fcd
Mirumo <mysteryperson520@gmail.com>
2021-08-23 22:43:41 +0800

    add another component

--- App.js
+++ App.js
@@ -2,10 +2,11 @@
 import React from 'react';
 
 import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 	console.log('App executed!');
 
-	return <WelcomeScreen />;
+	return <ViewImageScreen />;
 }
