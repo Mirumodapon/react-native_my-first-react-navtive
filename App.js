a4ea91a971b71cc4e5fe21b5b2a7b6ee56191c9f
Mirumo <mysteryperson520@gmail.com>
2021-08-23 21:14:33 +0800

    add alert box

--- App.js
+++ App.js
@@ -2,15 +2,28 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { Button } from 'react-native';
+import { Alert } from 'react-native';
 
 export default function App() {
 	console.log('App executed!');
 
-	const handlePress = () => console.log('Button pressed');
+	const handlePress = () =>
+		Alert.alert('Title', 'Message', [
+			{ text: 'Yes', onPress: () => console.log('yes pressed') },
+			{ text: 'No', onPress: () => console.log('no pressed') }
+		]);
+
+	const handlePress2 = () =>
+		Alert.prompt('Title', 'Message', (text) => console.log(text));
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<Button title="Click me" onPress={handlePress} color="orange" />
+			<Button
+				title="Click me - alert"
+				onPress={handlePress}
+				color="orange"
+			/>
+			<Button title="Click me - prompt" onPress={handlePress2} />
 		</SafeAreaView>
 	);
 }
