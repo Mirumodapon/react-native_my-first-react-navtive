8a211091db4496dc8f8dbee4efcd7faccf0af0d9
Mirumo <mysteryperson520@gmail.com>
2021-08-23 21:47:46 +0800

    detecting orientation changes

--- App.js
+++ App.js
@@ -1,32 +1,27 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
-import { Button } from 'react-native';
-import { Alert } from 'react-native';
-
-import { Platform } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Dimensions } from 'react-native';
 import { StatusBar } from 'react-native';
+
+import {
+	useDimensions,
+	useDeviceOrientation
+} from '@react-native-community/hooks';
 
 export default function App() {
 	console.log('App executed!');
 
-	const handlePress = () =>
-		Alert.alert('Title', 'Message', [
-			{ text: 'Yes', onPress: () => console.log('yes pressed') },
-			{ text: 'No', onPress: () => console.log('no pressed') }
-		]);
-
-	const handlePress2 = () =>
-		Alert.prompt('Title', 'Message', (text) => console.log(text));
+	const { landscape } = useDeviceOrientation();
 
 	return (
 		<SafeAreaView style={[styles.container, containerStyle]}>
-			<Button
-				title="Click me - alert"
-				onPress={handlePress}
-				color="white"
-			/>
-			<Button title="Click me - prompt" onPress={handlePress2} />
+			<View
+				style={{
+					backgroundColor: 'dodgerblue',
+					width: '100%',
+					height: landscape ? '100%' : '30%'
+				}}
+			></View>
 		</SafeAreaView>
 	);
 }
