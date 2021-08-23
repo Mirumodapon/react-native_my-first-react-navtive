cf17933b3b5db0b968d4e8c5bfe99bef8d25f374
Mirumo <mysteryperson520@gmail.com>
2021-08-23 22:09:09 +0800

    flex wrap and align content

--- App.js
+++ App.js
@@ -12,28 +12,43 @@ export default function App() {
 				backgroundColor: 'white',
 				flex: 1,
 				flexDirection: 'row',
-				justifyContent: 'space-evenly',
-				alignItems: 'center'
+				justifyContent: 'center',
+				alignItems: 'center',
+				alignContent: 'center',
+				flexWrap: 'wrap'
 			}}
 		>
 			<View
 				style={{
 					backgroundColor: 'dodgerblue',
 					width: 100,
-					height: 120,
-					alignSelf: 'flex-end'
+					height: 100
 				}}
 			/>
 			<View
 				style={{
 					backgroundColor: 'gold',
 					width: 100,
-					height: 180
+					height: 100
 				}}
 			/>
 			<View
 				style={{
 					backgroundColor: 'tomato',
+					width: 100,
+					height: 100
+				}}
+			/>
+			<View
+				style={{
+					backgroundColor: 'gray',
+					width: 100,
+					height: 100
+				}}
+			/>
+			<View
+				style={{
+					backgroundColor: 'green',
 					width: 100,
 					height: 100
 				}}
