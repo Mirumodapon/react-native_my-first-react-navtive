e51708db0bc50d4c0d1e1029eb9efc8d802c06f8
Mirumo <mysteryperson520@gmail.com>
2021-08-25 16:26:10 +0800

    border

--- App.js
+++ App.js
@@ -2,11 +2,26 @@
 import React from 'react';
 
 import { View } from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+// import ViewImageScreen from './app/screens/ViewImageScreen';
+// import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 	console.log('App executed!');
 
-	return <ViewImageScreen />;
+	return (
+		<View
+			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+		>
+			<View
+				style={{
+					backgroundColor: 'dodgerblue',
+					width: 100,
+					height: 100,
+					borderWidth: 10,
+					borderColor: 'royalblue',
+					borderRadius: '50%'
+				}}
+			></View>
+		</View>
+	);
 }
