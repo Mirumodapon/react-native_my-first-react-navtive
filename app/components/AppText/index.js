636f0d687a0c79527f3b01a2e565b44d6b6393fd
Mirumo <mysteryperson520@gmail.com>
2021-08-25 17:02:06 +0800

    organizing style

---
+++ app/components/AppText/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import styles from './styles';
+
+export default ({ children }) => {
+	return <Text style={styles.text}>{children}</Text>;
+};
