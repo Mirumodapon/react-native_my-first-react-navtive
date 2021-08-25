42eb97a09c1c20e47b5a603e02eb2690e9822140
Mirumo <mysteryperson520@gmail.com>
2021-08-25 23:55:35 +0800

    flat list

--- App.js
+++ App.js
@@ -8,8 +8,10 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
+import MessagesScreen from './app/screens/MessagesScreen';
+
 export default function App() {
 	console.log('App executed!');
 
-	return <ViewImageScreen />;
+	return <MessagesScreen />;
 }
